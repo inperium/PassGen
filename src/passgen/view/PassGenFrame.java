dbb2c6ec42dbda804cdffe2e729a0df8e71f7952
inperium <ibow9416@740s-pl-111.local>
2016-12-08 12:30:33 -0700

    Working the on display.

--- src/passgen/view/PassGenFrame.java
+++ src/passgen/view/PassGenFrame.java
@@ -1,12 +1,33 @@
 package passgen.view;
 
+import javax.swing.JFrame;
 import passgen.controller.PassGenController;
 
-public class PassGenFrame {
+public class PassGenFrame extends JFrame
+{
+	private PassGenController baseController;
+	private PassGenPanel passPanel;
 
 	public PassGenFrame(PassGenController passGenController) 
 	{
-		
+			super();
+			this.baseController = baseController;
+			this.passPanel = new PassGenPanel(baseController);
+			
+			this.setupFrame();
+	}
+
+	private void setupFrame() 
+	{
+		this.setContentPane(passPanel);
+		this.setTitle("PassGen");
+		this.setSize(600, 970);
+		this.setVisible(true);
+	}
+	
+	public PassGenPanel getPassPanel()
+	{
+		return this.passPanel;
 	}
 
 }
--- src/passgen/view/PassGenPanel.java
+++ src/passgen/view/PassGenPanel.java
@@ -1,5 +1,47 @@
 package passgen.view;
 
-public class PassGenPanel {
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
+
+import passgen.controller.PassGenController;
+
+public class PassGenPanel extends JPanel
+{
+	private PassGenController baseController;
+	private SpringLayout baseLayout;
+	private JTextArea passwordDisplay;
+	private JSlider lengthSlider;
+
+	public PassGenPanel(PassGenController baseController) 
+	{
+		super();
+		this.baseController = baseController;
+		baseLayout= new SpringLayout();
+		passwordDisplay = new JTextArea(5,40);
+
+		setupPanel();
+		setupLayout();
+		setupListners();
+		
+	}
+
+	private void setupListners() {
+		this.setLayout(baseLayout);
+		this.setBackground(new Color ());
+
+		
+	}
+
+	private void setupLayout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setupPanel() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
